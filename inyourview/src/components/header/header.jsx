15dfd174dc3ee368f3d1bc25c4d606332c583dd8
Junnna <gud0415@gmail.com>
2021-01-01 14:13:19 +0900

    fix : edit nav bar design

--- inyourview/src/components/header/header.jsx
+++ inyourview/src/components/header/header.jsx
@@ -127,13 +127,6 @@ function Header(props) {
       )}
       {isSmallScreen && (
         <div>
-          {/* <button className={styles.close} onClick={onToggle} open={open}>
-            <span
-              className={
-                open ? `${styles.bar} ${styles.active}` : `${styles.bar}`
-              }
-            ></span>
-          </button> */}
           <button
             className={
               open
@@ -148,78 +141,80 @@ function Header(props) {
             <span> </span>
           </button>
           {open && (
-            <ul className={styles.ul}>
-              <li className={styles.li}>
-                <div onMouseOver={hover1} onMouseOut={hout1}>
-                  <button className={styles.button}>인유어뷰</button>
-                  <ul id="hover1" className={styles.sevul}>
-                    <Link to="/intro">
-                      <li className={styles.sevli}>
-                        <button className={`${styles.button} ${styles.btn}`}>
-                          소개
-                        </button>
-                      </li>
-                    </Link>
-                    <Link to="/portfolio">
-                      <li className={styles.sevli}>
-                        <button className={`${styles.button} ${styles.btn}`}>
-                          포트폴리오
-                        </button>
-                      </li>
-                    </Link>
-                    <Link to="/way">
-                      <li className={styles.sevli}>
-                        <button className={`${styles.button} ${styles.btn}`}>
-                          찾아오는 길
-                        </button>
-                      </li>
-                    </Link>
-                  </ul>
-                </div>
-              </li>
-              <li className={styles.li}>
-                <div onMouseOver={hover2} onMouseOut={hout2}>
-                  <button className={styles.button}>가이드</button>
-                  <ul id="hover2" className={styles.sevul}>
-                    <Link to="/list">
-                      <li className={styles.sevli}>
-                        <button className={`${styles.button} ${styles.btn}`}>
-                          서비스 목록
-                        </button>
-                      </li>
-                    </Link>
-                    <Link to="/procedure">
-                      <li className={styles.sevli}>
-                        <button className={`${styles.button} ${styles.btn}`}>
-                          진행절차
-                        </button>
-                      </li>
-                    </Link>
-                  </ul>
-                </div>
-              </li>
-              <li className={styles.li}>
-                <div onMouseOver={hover3} onMouseOut={hout3}>
-                  <button className={styles.button}>문의</button>
-                  <ul id="hover3" className={styles.sevul}>
-                    <Link to="/notice">
-                      <li className={styles.sevli}>
-                        <button className={`${styles.button} ${styles.btn}`}>
-                          공지사항
-                        </button>
-                      </li>
-                    </Link>
-                    <Link to="/qna">
-                      <li className={styles.sevli}>
-                        <button className={`${styles.button} ${styles.btn}`}>
-                          게시판
-                        </button>
-                      </li>
-                    </Link>
-                  </ul>
-                </div>
-              </li>
-            </ul>
+            <div className={styles.wrap}>
+              <ul className={styles.ul}>
+                <li className={styles.li}>
+                  <div onMouseOver={hover1} onMouseOut={hout1}>
+                    <button className={styles.button}>인유어뷰</button>
+                    <ul id="hover1" className={styles.sevul}>
+                      <Link to="/intro">
+                        <li className={styles.sevli}>
+                          <button className={`${styles.button} ${styles.btn}`}>
+                            소개
+                          </button>
+                        </li>
+                      </Link>
+                      <Link to="/portfolio">
+                        <li className={styles.sevli}>
+                          <button className={`${styles.button} ${styles.btn}`}>
+                            포트폴리오
+                          </button>
+                        </li>
+                      </Link>
+                      <Link to="/way">
+                        <li className={styles.sevli}>
+                          <button className={`${styles.button} ${styles.btn}`}>
+                            찾아오는 길
+                          </button>
+                        </li>
+                      </Link>
+                    </ul>
+                  </div>
+                </li>
+                <li className={styles.li}>
+                  <div onMouseOver={hover2} onMouseOut={hout2}>
+                    <button className={styles.button}>가이드</button>
+                    <ul id="hover2" className={styles.sevul}>
+                      <Link to="/list">
+                        <li className={styles.sevli}>
+                          <button className={`${styles.button} ${styles.btn}`}>
+                            서비스 목록
+                          </button>
+                        </li>
+                      </Link>
+                      <Link to="/procedure">
+                        <li className={styles.sevli}>
+                          <button className={`${styles.button} ${styles.btn}`}>
+                            진행절차
+                          </button>
+                        </li>
+                      </Link>
+                    </ul>
+                  </div>
+                </li>
+                <li className={styles.li}>
+                  <div onMouseOver={hover3} onMouseOut={hout3}>
+                    <button className={styles.button}>문의</button>
+                    <ul id="hover3" className={styles.sevul}>
+                      <Link to="/notice">
+                        <li className={styles.sevli}>
+                          <button className={`${styles.button} ${styles.btn}`}>
+                            공지사항
+                          </button>
+                        </li>
+                      </Link>
+                      <Link to="/qna">
+                        <li className={styles.sevli}>
+                          <button className={`${styles.button} ${styles.btn}`}>
+                            게시판
+                          </button>
+                        </li>
+                      </Link>
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+            </div>
           )}
         </div>
       )}
